f5741b630021334e10ccfc00a384283b90566188
Sanchit kumar <sanchit77kumar@gmail.com>
2022-05-18 14:02:13 +0530

    Program1_PracticeJAVAConcepts_Sanchit

--- src/javaProject/Program1.java
+++ src/javaProject/Program1.java
@@ -3,6 +3,6 @@ package javaProject;
 public class Program1 {
 
 	public static void main(String[] args) {
-		System.out.println("PracticeJAVAConcepts-Repo Upload");
+		System.out.println("PracticeJAVAConcepts-Project Upload");
 	}
 }
